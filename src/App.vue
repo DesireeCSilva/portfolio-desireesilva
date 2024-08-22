2c00dccea380e2cfbf06e214b50feb506f2eaf53
nuria <nuria.martinez@gmail.com>
2024-08-22 18:46:24 +0200

    basic structure
    new file:   .gitignore
    modified:   README.md
    new file:   babel.config.js
    new file:   jsconfig.json
    new file:   package-lock.json
    new file:   package.json
    new file:   public/favicon.ico
    new file:   public/index.html
    new file:   src/App.vue
    new file:   src/assets/logo.png
    new file:   src/components/AppFooter.vue
    new file:   src/components/AppHeader.vue
    new file:   src/components/HelloWorld.vue
    new file:   src/main.js
    new file:   vue.config.js

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <AppHeader/>
+    <AppFooter />
+  </main>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+
+
+export default {
+  name: 'index_page',
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+  min-height: 100vh;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <nav class="header_portfolio">
+      <ul id="main-menu" class="main-menu">
+        <li class="main-menu_item">
+          <img
+            src="./assets/logo_desiree.png"
+            alt="logotipo de Desirée Silva"
+          />
+        </li>
+        <li class="main-menu_item">
+          <a href="/index.html#section-01" class="main-menu_link">Sobre mí</a>
+        </li>
+        <li class="main-menu_item">
+          <a href="/index.html#section-02" class="main-menu_link">Proyectos</a>
+        </li>
+        <li class="main-menu_item">
+          <a href="/index.html#section-03" class="main-menu_link">Formación</a>
+        </li>
+        <button class="button_header">
+          <img class="icon_header" src="assets/icons/call.png" alt="" /><a
+            href="tel:675937952"
+            >¡Contáctame!</a
+          >
+        </button>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
