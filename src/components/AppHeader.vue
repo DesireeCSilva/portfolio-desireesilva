b9d9f6409539adef8118308587af9e5b1cac9dea
nuria <nuria.martinez@gmail.com>
2024-08-22 19:28:49 +0200

    updated url to assets
    modified:   public/index.html
    modified:   src/components/AppHeader.vue
    modified:   src/main.js

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,7 +4,7 @@
       <ul id="main-menu" class="main-menu">
         <li class="main-menu_item">
           <img
-            src="./assets/logo_desiree.png"
+            src="../assets/logo_desiree.png"
             alt="logotipo de Desirée Silva"
           />
         </li>
@@ -18,7 +18,7 @@
           <a href="/index.html#section-03" class="main-menu_link">Formación</a>
         </li>
         <button class="button_header">
-          <img class="icon_header" src="assets/icons/call.png" alt="" /><a
+          <img class="icon_header" src="../assets/icons/call.png" alt="" /><a
             href="tel:675937952"
             >¡Contáctame!</a
           >
@@ -29,7 +29,95 @@
 </template>
 
 <script>
-export default {};
 </script>
 
-<style></style>
+<style lang="scss">
+.header_portfolio {
+  display: flex;
+  height: 10rem;
+  align-items: center;
+  justify-content: space-around;
+  
+  .main-menu {
+    list-style: none;
+    display: flex;
+    text-align: center;
+    align-items: center;
+    align-content: center;
+    justify-content: space-between;
+    text-decoration: none;
+    cursor: pointer;
+
+    &_item {
+      padding: 3em;
+      font-size: 1rem;
+      font-weight: 500;
+      cursor: pointer;
+      border: none;
+    }
+
+    &_link {
+      text-decoration: none;
+      color: #000000;    
+      position: relative;
+      color: inherit;
+    }
+
+    &_link::before {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 2px;
+      background: linear-gradient(to right, #CE93D8, #e878a2, #eb85ab);
+      z-index: 1;
+      transform: scaleX(0);
+      transform-origin: left;
+      transition: transform 0.5s ease-in-out;
+    }
+
+    &_link:hover::before {
+      transform: scaleX(1);
+    }
+
+    .button_header {
+      background-color: #CE93D8;
+      width: 10em;
+      height: fit-content;
+      font-size: 1.3rem;
+      font-family: 'Poppins';
+      padding-top: 0.5rem;
+      padding-bottom: 0.5rem;
+      border-radius: 0.50rem;
+      border-style: none;
+      cursor: pointer; 
+      text-decoration: none;
+      text-align: center;
+      justify-content: center;
+      display: flex;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); 
+      transition: all 0.3s ease; 
+
+      .icon_header {
+        display: flex;
+        flex-direction: row;
+        align-self: center;
+        width: 1.2rem;
+        height: 1.2rem;
+      }
+
+      a {
+        text-decoration: none;
+        color: #000000;
+      }
+    }
+
+    .button_header:hover {
+      background-color: #CE93D8;
+      transform: scale(0.95); 
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2); 
+    }
+  }
+}
+</style>
