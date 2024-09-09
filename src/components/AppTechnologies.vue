8fc96c055665d6a4a285cbf2ab9a698a56090956
DesireeCSilva <desiresilva1987@gmail.com>
2024-09-10 00:48:37 +0200

    add icons from library iconify

--- src/components/AppTechnologies.vue
+++ src/components/AppTechnologies.vue
@@ -4,36 +4,36 @@
             <h1 class="title_sidebar">Tecnologías</h1>
             <div class="icons">
                 <div class="icon-wrapper">
-                    <img src="@/assets/icons/Figma.png" alt="icono de Figma" />
+                    <img width="100" height="100" src="https://img.icons8.com/ios-glyphs/100/figma.png" alt="figma"/>
                     <p class="icon-label">Figma</p>
                 </div>
                 <div class="icon-wrapper">
-                    <img src="@/assets/icons/Html 5.png" alt="icono de HTML" />
-                    <p class="icon-label">HTML</p>
+                    <img width="100" height="100" src="https://img.icons8.com/ios-glyphs/100/html-5.png" alt="html-5"/>    
+                    <p class="icon-label">HTML5</p>
                 </div>
                 <div class="icon-wrapper">
-                    <img src="@/assets/icons/CSS3.png" alt="icono de CSS" />
-                    <p class="icon-label">CSS</p>
+                    <img width="100" height="100" src="https://img.icons8.com/ios-glyphs/100/css3.png" alt="css3"/>
+                    <p class="icon-label">CSS3</p>
                 </div>
                 <div class="icon-wrapper">
-                    <img src="@/assets/icons/JavaScript.png" alt="icono de JavaScript" />
+                    <img width="100" height="100" src="https://img.icons8.com/ios-glyphs/100/javascript.png" alt="javascript"/>
                     <p class="icon-label">JavaScript</p>
                 </div>
                 <div class="icon-wrapper">
-                    <img src="@/assets/GitHub-big.png" alt="icono de GitHub" />
+                    <img width="100" height="100" src="https://img.icons8.com/ios-glyphs/100/github.png" alt="github"/>
                     <p class="icon-label">GitHub</p>
                 </div>
                 <div class="icon-wrapper">
-                    <img src="@/assets/icons/Git.png" alt="icono de Git" />
+                    <img width="100" height="100" src="https://img.icons8.com/ios-filled/100/git.png" alt="git"/>
                     <p class="icon-label">Git</p>
                 </div>
                 <div class="icon-wrapper">
-                    <img src="@/assets/icons/React Native.png" alt="icono de React" />
+                    <img width="100" height="100" src="https://img.icons8.com/ios-glyphs/100/react.png" alt="react"/> 
                     <p class="icon-label">React</p>
                 </div>
                 <div class="icon-wrapper">
-                    <img src="@/assets/icons/vue.png" alt="icono de Vue" />
-                    <p class="icon-label">Vue</p>
+                    <img width="100" height="100" src="https://img.icons8.com/ios-glyphs/100/vuejs.png" alt="vuejs"/>
+                    <p class="icon-label">Vue.js</p>
                 </div>
             </div>
         </div>
@@ -41,11 +41,22 @@
     <hr class="hr_portfolio" />
 </template>
 
+
 <script>
 export default {};
 </script>
 
 <style>
+
+        .iconify {
+            font-size: 50px; /* Tamaño uniforme para los íconos */
+            color: #333;     /* Color de los íconos */
+            transition: transform 0.3s ease-in-out;
+        }
+
+        .iconify:hover {
+            transform: rotate(360deg); /* Animación al pasar el cursor */
+        }
 .content_section-04 {
     display: flex;
     flex-direction: row;
@@ -78,19 +89,15 @@ export default {};
     opacity: 0;
     transition: opacity 0.3s ease-in-out;
     position: absolute;
-    bottom: -2rem;
+    bottom: 2rem;
     font-size: 0.9rem;
     font-weight: bold;
-    color: #333;
+    color: #000000;
+    bottom: -2rem;
 }
 
 .icon-wrapper:hover .icon-label {
     opacity: 1;
-}
-
-.icons img {
-    transition: transform 0.2s ease-in-out;
-    width: 50px; /* Ajusta el tamaño según lo necesites */
 }
 
 .icons img:hover {
@@ -99,6 +106,11 @@ export default {};
     transform: rotateY(180deg);
     transform-style: preserve-3d;
     transition: transform 0.4s ease-in-out;
+}
+
+.icons i {
+    font-size: 50px; /* Ajusta el tamaño del icono según sea necesario */
+    transition: transform 0.2s ease-in-out;
 }
 
 /*Media Queries */
@@ -121,7 +133,7 @@ export default {};
 
 @media only screen and (min-width: 1920px) {
     .content_section-04 {
-        padding: 1rem 5rem 0rem 1rem;
+        padding: 1rem 5rem 2rem 1rem;
         align-items: center;
         justify-content: space-between;
     }
