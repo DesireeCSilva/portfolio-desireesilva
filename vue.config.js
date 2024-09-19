db68cd05c1583d4eb2640dca29e097575edb79cf
DesireeCSilva <desireecarrizosasilva@gmail.com>
2024-09-19 18:09:03 +0200

    change publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/portfolio_desiree_silva/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio-desireesilva/" : "/",
 })
